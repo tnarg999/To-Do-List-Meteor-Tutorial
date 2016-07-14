77ff7b0c2ba2feb1db51cc5d4c71be50f2887a4c
tnarg999 <tnargmaster@gmail.com>
2016-07-14 12:41:30 -0700

    Initial Commit
    Meteor Tutorial

---
+++ client/main.js
@@ -0,0 +1,5 @@
+import '../imports/startup/accounts-config.js';
+import '../imports/ui/body.js';
+
+
+
---
+++ imports/startup/accounts-config.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import { Accounts } from 'meteor/accounts-base';
+
+Accounts.ui.config({
+	passwordSignupFields: 'USERNAME_ONLY',
+});
